759e33b0a9c43a79c539857bd77f00b1159b0b8d
Olexander <olexander.sj@gmail.com>
2018-10-04 20:56:25 +0300

    First test of webpack

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import "./index.css";
+import createButton from "./button";
+import colorElement from "./colorElement";
+
+const div = document.createElement("div");
+div.innerText = "Hello World";
+div.style.color = "red";
+
+const button = createButton("HEY FIRST BUTTON YO!");
+
+button.addEventListener("click", e => {
+  colorElement(div, "cyan");
+});
+
+document.body.appendChild(button);
+document.body.appendChild(div);
